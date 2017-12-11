029ba9066d9f60994858acb0229e790d7140735a
vankizzle <blaidzer@abv.bg>
2017-12-11 20:34:44 +0100

    Update pipe.c

--- Pipes/pipe.c
+++ Pipes/pipe.c
@@ -3,20 +3,21 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdio.h>
-
+#define MAX_BUFF 256
 int main(){
 
-int fd;
-char buff[256];
+int fd,i;
+char buff[MAX_BUFF];
+char* tmp;
 char* myfifo = "/tmp/myfifo";
 mkfifo(myfifo,0666);
 printf("Run Reader process to read the FIFO File\n");
 fd = open(myfifo,O_WRONLY);
-write(fd,"This is a test",sizeof("This is a test"));
+for(i = 0;i < MAX_BUFF;i++){
+ tmp = "testing";
+ write(fd,tmp,sizeof(tmp));
+}
 close(fd);
 unlink(myfifo);
-
-
-
 return 0;
 }
