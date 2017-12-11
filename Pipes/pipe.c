9097193dcbbe7233b8247a0602e029aa2a58fa4e
vankizzle <blaidzer@abv.bg>
2017-12-11 21:11:37 +0100

    Update pipe.c

--- Pipes/pipe.c
+++ Pipes/pipe.c
@@ -9,14 +9,20 @@ int main(){
 int fd,i;
 char buff[MAX_BUFF];
 char* tmp;
+tmp = "testing ";
 char* myfifo = "/tmp/myfifo";
 mkfifo(myfifo,0666);
 printf("Run Reader process to read the FIFO File\n");
 fd = open(myfifo,O_WRONLY);
-for(i = 0;i < MAX_BUFF;i++){
- tmp = "testing";
- write(fd,tmp,sizeof(tmp));
-}
+//like this it will write untill EOF,CTRL  + C
+ while(1){
+  write(fd,tmp,sizeof(tmp));
+ }
+ //like this it will write till MAX_BUFF
+ //for(i = 0;i < MAX_BUFF;i++){
+//write(fd,tmp,sizeof(tmp));
+ //}
+ 
 close(fd);
 unlink(myfifo);
 return 0;
